ad045435dd2d3c5da0c66e281d128a840d34d916
nonya business <fremed@gmail.com>
2020-01-09 20:36:25 -0800

    initial

--- StoreGetter.py
+++ StoreGetter.py
@@ -2,32 +2,43 @@ import xml.etree.cElementTree as etree
 import xml.etree.ElementTree as ET
 import pandas as pd
 
+'''Get server name from save file'''
+tree = ET.parse('save/Sandbox.sbc')
+root = tree.getroot()
+Server = (root.find("SessionName").text)
+
+'''Now parse the other save file for store data'''
 tree = ET.parse('save/SANDBOX_0_0_0_.sbs')
 root = tree.getroot()
 
 rows=[]
 
+#Get all entities
 for grid in root.iter('MyObjectBuilder_EntityBase'):
+    #If the entity is a grid
     if grid.attrib['{http://www.w3.org/2001/XMLSchema-instance}type'] == "MyObjectBuilder_CubeGrid":
-        grid_name = grid.find("DisplayName").text
-        blocks = grid.find("CubeBlocks")
-        position_data = grid.find("PositionAndOrientation").find("Position")
-        x=position_data.attrib['x']
-        y=position_data.attrib['y']
-        z=position_data.attrib['z']
+
+        grid_name = grid.find("DisplayName").text #The grid name
+        blocks = grid.find("CubeBlocks") #The blocks in the grid
+        position_data = grid.find("PositionAndOrientation").find("Position") #The position of the grid
+        x = position_data.attrib['x']
+        y = position_data.attrib['y']
+        z = position_data.attrib['z']
 
         for cube in blocks:
-
+            #If the block is a store
             if cube.attrib['{http://www.w3.org/2001/XMLSchema-instance}type'] == "MyObjectBuilder_StoreBlock":
-                items = cube.find("PlayerItems").findall("MyObjectBuilder_StoreItem")
-                owner = cube.find("Owner").text
+                items = cube.find("PlayerItems").findall("MyObjectBuilder_StoreItem") #Get all the items
+                owner = cube.find("Owner").text #Get the items
                 for item in items:
-                    item_name=item.find("Item").attrib["Subtype"]
-                    item_type = item.find("StoreItemType").text
-                    price_per_unit=item.find("PricePerUnit").text
-                    quantity = item.find("Amount").text
+                    item_name=item.find("Item").attrib["Subtype"] #The item name
+                    item_type = item.find("StoreItemType").text #Offer or order
+                    price_per_unit=item.find("PricePerUnit").text #Price per unit
+                    quantity = item.find("Amount").text #Qty
 
+                    #This is a neat trick for making dataframes. It's a list of dictionaries
                     rows.append({
+                        "Server":Server,
                         "Grid Name":grid_name,
                         "X":x,
                         "Y":y,
@@ -39,15 +50,10 @@ for grid in root.iter('MyObjectBuilder_EntityBase'):
                         "Price per unit":price_per_unit
                     }
                     )
+
+#Make a dataframe
 df = pd.DataFrame(rows)
+
+#Save a dataframe
 df.to_csv("test.csv")
 
-# for cube in root.iter('MyObjectBuilder_CubeBlock'):
-#     if cube.attrib['{http://www.w3.org/2001/XMLSchema-instance}type']=="MyObjectBuilder_StoreBlock":
-#         items=cube.find("PlayerItems").findall("MyObjectBuilder_StoreItem")
-#         for item in items:
-#             print(item.find("Item").attrib["Subtype"])
-#             print(item.find("StoreItemType").text)
-#             print(item.find("PricePerUnit").text)
-#             print(item.find("Amount").text)
-#
